4ecd41d99e999041394646fc956acdfeac120605
deathmaskXE <deathmask2014@gmail.com>
2026-07-08 06:28:05 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAKlkNp8YuJ4yL9B2AkDGymjS82re253OQ",
+  authDomain: "xe-tester-analytics.firebaseapp.com",
+  projectId: "xe-tester-analytics",
+  storageBucket: "xe-tester-analytics.firebasestorage.app",
+  messagingSenderId: "223479280184",
+  appId: "1:223479280184:web:bc4b66eb2bebca17682ccd"
+};
